11fee8c9d68a9833ace4180502e381a013fd50e7
Pedro Cunial <pedrocc4@al.insper.edu.br>
2016-04-01 08:25:03 -0300

    Criei este branch para estudar como os recursos visuais (swing, Jpanel etc) funcionam, assim que me sentir seguro com isso eu passo tudo pro master e deleto esse branch

---
+++ src/main/java/desagil/vision/Screen.java
@@ -0,0 +1,17 @@
+package desagil.vision;
+
+import javax.swing.*;
+import java.awt.*;
+
+
+public class Screen {
+	
+	// Estou fazendo um teste para aprender a usar J
+	private JFrame frame;
+	private JPanel panel;
+	private JButton buttonA;
+	private JLabel labelA;
+	
+	
+
+}
