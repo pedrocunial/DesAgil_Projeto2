98f4b5037ad9cdc5e27e41a18803db8db15fdbdc
Pedro Cunial <pedrocc4@al.insper.edu.br>
2016-04-01 14:00:10 -0300

    Aprendi a mexer itens da janela, fiz alguns exemplos, mas estou com um bug que a janela abre vazia, mas se maximizo ela fica tudo ok (depois ela pode ser minimizada e terá as configurações expostas)

--- src/main/java/desagil/vision/Screen.java
+++ src/main/java/desagil/vision/Screen.java
@@ -12,46 +12,52 @@ public class Screen implements ActionListener{
 	// https://www.youtube.com/watch?v=GvTZ2Huo0T4 << Tutorial que estou usando
 	// e suas respectivas continuações
 	
-	private JFrame frame;
+	private static JFrame frame;
 	private JPanel panel;
 	private JButton buttonSelecionar;
-	private JButton buttonOr;
-	private JLabel labelA;
+	private JLabel frameTitle;
 	private JComboBox<Object> comboBox; // Sugestão do Matheus Dias
 	private String[] portas = {"AND", "OR", "NOT", "XOR", "HalfAdder", "FullAdder"};
 	
 	public Screen() {
-		gui();
-	
-	}
-
-
-	public void gui() {
-		 comboBox = new JComboBox<Object>(portas);
-		 comboBox.setSelectedIndex(0);
-		 comboBox.addActionListener(this);
-		 
-		 frame = new JFrame("Simulador de Portas Lógicas");
-		 frame.setVisible(true);
-		 frame.setSize(600, 400); // (Largura, altura)
-		 frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		 
-		 panel = new JPanel();
-		 panel.setBackground(Color.WHITE);
-		 
-		 buttonSelecionar = new JButton("AND");
-		 buttonOr = new JButton("OR");
-		 labelA = new JLabel("Escolha uma porta");		 
+		frame = new JFrame("Simulador de Portas Lógicas");
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		Insets insets = frame.getInsets();
+		frame.setSize(300 + insets.left + insets.right,
+	              150 + insets.top + insets.bottom);
 		
-		 panel.add(buttonSelecionar);
-		 panel.add(buttonOr);
-		 panel.add(labelA);
-		 panel.add(comboBox);
-		 
-		 frame.add(panel);
-		 
-	}
+		comboBox = new JComboBox<Object>(portas);
+		comboBox.setSelectedIndex(0);
+		comboBox.addActionListener(this);
+				
+		panel = new JPanel();
+		panel.setBackground(Color.WHITE);
+		panel.setLayout(null);
+		
+		buttonSelecionar = new JButton("Selecionar");
+		
+		frameTitle = new JLabel("Escolha uma porta");		 
 	
+		panel.add(buttonSelecionar);
+		panel.add(frameTitle);
+		panel.add(comboBox);
+		
+		Dimension size = buttonSelecionar.getPreferredSize();
+		buttonSelecionar.setBounds(20 + insets.left,
+				20 + insets.top, size.width, size.height);
+		size = comboBox.getPreferredSize();
+		comboBox.setBounds(25 + insets.left,
+				50 + insets.top, size.width, size.height);
+		size = frameTitle.getPreferredSize();
+		frameTitle.setBounds(12 + insets.left,
+				insets.top, size.width, size.height);
+		
+		frame.add(panel);
+			
+	}
+
+
 	public static void main(String[] args) {
 		new Screen();
 	}
