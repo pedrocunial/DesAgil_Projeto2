3c8ea97c1bb5d89443fb6f0546de5eef0f4175ca
Pedro Cunial <pedrocc4@al.insper.edu.br>
2016-04-01 09:32:36 -0300

    Segui a sugestão do Matheus Dias de usar JComboBox, gostei bastante do resultado

--- src/main/java/desagil/vision/Screen.java
+++ src/main/java/desagil/vision/Screen.java
@@ -2,9 +2,11 @@ package desagil.vision;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 
-public class Screen {
+public class Screen implements ActionListener{
 	
 	// Estou fazendo um teste para aprender a usar JFrame, Panel etc
 	// https://www.youtube.com/watch?v=GvTZ2Huo0T4 << Tutorial que estou usando
@@ -12,15 +14,23 @@ public class Screen {
 	
 	private JFrame frame;
 	private JPanel panel;
-	private JButton buttonA;
+	private JButton buttonSelecionar;
+	private JButton buttonOr;
 	private JLabel labelA;
+	private JComboBox<Object> comboBox; // Sugestão do Matheus Dias
+	private String[] portas = {"AND", "OR", "NOT", "XOR", "HalfAdder", "FullAdder"};
 	
 	public Screen() {
 		gui();
 	
 	}
 
+
 	public void gui() {
+		 comboBox = new JComboBox<Object>(portas);
+		 comboBox.setSelectedIndex(0);
+		 comboBox.addActionListener(this);
+		 
 		 frame = new JFrame("Simulador de Portas Lógicas");
 		 frame.setVisible(true);
 		 frame.setSize(600, 400); // (Largura, altura)
@@ -29,18 +39,27 @@ public class Screen {
 		 panel = new JPanel();
 		 panel.setBackground(Color.WHITE);
 		 
-		 buttonA = new JButton("Test");
+		 buttonSelecionar = new JButton("AND");
+		 buttonOr = new JButton("OR");
 		 labelA = new JLabel("Escolha uma porta");		 
-		 
-		 panel.add(buttonA);
+		
+		 panel.add(buttonSelecionar);
+		 panel.add(buttonOr);
 		 panel.add(labelA);
+		 panel.add(comboBox);
 		 
-		 frame.add(panel, BorderLayout.SOUTH);
+		 frame.add(panel);
 		 
 	}
 	
 	public static void main(String[] args) {
 		new Screen();
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
 	}
 	
 }
