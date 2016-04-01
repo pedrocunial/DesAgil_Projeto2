4bcc54046228e8704c4bcfd851ee3e51e80f032c
Pedro Cunial <pedrocc4@al.insper.edu.br>
2016-04-01 08:39:16 -0300

    Criei uma janela, nada de mais, mas já dando os primeiros passos, coloquei o link da série do youtube que estou usando como comentário no código para dar o devido crédito

--- src/main/java/desagil/vision/Screen.java
+++ src/main/java/desagil/vision/Screen.java
@@ -6,12 +6,41 @@ import java.awt.*;
 
 public class Screen {
 	
-	// Estou fazendo um teste para aprender a usar J
+	// Estou fazendo um teste para aprender a usar JFrame, Panel etc
+	// https://www.youtube.com/watch?v=GvTZ2Huo0T4 << Tutorial que estou usando
+	// e suas respectivas continuações
+	
 	private JFrame frame;
 	private JPanel panel;
 	private JButton buttonA;
 	private JLabel labelA;
 	
+	public Screen() {
+		gui();
 	
+	}
 
+	public void gui() {
+		 frame = new JFrame("Simulador de Portas Lógicas");
+		 frame.setVisible(true);
+		 frame.setSize(600, 400); // (Largura, altura)
+		 frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		 
+		 panel = new JPanel();
+		 panel.setBackground(Color.WHITE);
+		 
+		 buttonA = new JButton("Test");
+		 labelA = new JLabel("Escolha uma porta");		 
+		 
+		 panel.add(buttonA);
+		 panel.add(labelA);
+		 
+		 frame.add(panel, BorderLayout.SOUTH);
+		 
+	}
+	
+	public static void main(String[] args) {
+		new Screen();
+	}
+	
 }
